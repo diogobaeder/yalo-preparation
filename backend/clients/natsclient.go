4296b7a6fcb57c06779db5c712148b935423251c
Diogo Baeder <diogobaeder@yahoo.com.br>
2023-03-11 17:04:08 -0300

    Ability to prepare the client by adding a JetStream stream.

--- backend/clients/natsclient.go
+++ backend/clients/natsclient.go
@@ -4,15 +4,28 @@ import (
 	"errors"
 	"github.com/nats-io/nats.go"
 	"os"
+	"time"
 )
 
 // SimplifiedJetStream is a simplified version of nats.JetStreamContext containing only the methods we need.
 // This will help with mocking the interface in tests.
 type SimplifiedJetStream interface {
+	AddStream(cfg *nats.StreamConfig, opts ...nats.JSOpt) (*nats.StreamInfo, error)
 }
 
 type Client struct {
 	js SimplifiedJetStream
+}
+
+func (c *Client) Prepare() error {
+	duration, _ := time.ParseDuration("24h")
+	_, err := c.js.AddStream(&nats.StreamConfig{
+		Name:     "yalo",
+		Subjects: []string{"yalo.>"},
+		MaxAge:   duration,
+	})
+
+	return err
 }
 
 func NewNatsClient() (Client, error) {
--- backend/clients/natsclient_test.go
+++ backend/clients/natsclient_test.go
@@ -2,10 +2,22 @@ package clients
 
 import (
 	"github.com/nats-io/nats.go"
+	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 	"os"
 	"testing"
+	"time"
 )
+
+type JetStreamContextMock struct {
+	mock.Mock
+}
+
+func (j *JetStreamContextMock) AddStream(cfg *nats.StreamConfig, opts ...nats.JSOpt) (*nats.StreamInfo, error) {
+	_ = opts
+	args := j.Called(cfg)
+	return args.Get(0).(*nats.StreamInfo), nil
+}
 
 func TestStartsWithJetStream(t *testing.T) {
 	ensure := require.New(t)
@@ -18,4 +30,22 @@ func TestStartsWithJetStream(t *testing.T) {
 	ensure.Nilf(err, "Could not instantiate client: %v", err)
 	ensure.Implements((*SimplifiedJetStream)(nil), instance.js)
 	ensure.Implements((*nats.JetStreamContext)(nil), instance.js)
+}
+
+func TestPreparesClient(t *testing.T) {
+	ensure := require.New(t)
+	js := new(JetStreamContextMock)
+	instance := &Client{js}
+	duration, _ := time.ParseDuration("24h")
+	config := &nats.StreamConfig{
+		Name:     "yalo",
+		Subjects: []string{"yalo.>"},
+		MaxAge:   duration,
+	}
+	info := new(nats.StreamInfo)
+	js.On("AddStream", config).Return(info, nil)
+
+	_ = instance.Prepare()
+
+	ensure.True(js.AssertCalled(t, "AddStream", config))
 }
