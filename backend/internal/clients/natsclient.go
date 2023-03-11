591d64071fe234bd3e702a9b80f718bf122f4c92
Diogo Baeder <diogobaeder@yahoo.com.br>
2023-03-11 19:30:19 -0300

    Documentation.

--- backend/internal/clients/natsclient.go
+++ backend/internal/clients/natsclient.go
@@ -8,7 +8,7 @@ import (
 )
 
 // SimplifiedJetStream is a simplified version of nats.JetStreamContext containing only the methods we need.
-// This will help with mocking the interface in tests.
+// This helps with mocking the interface in tests.
 type SimplifiedJetStream interface {
 	AddStream(cfg *nats.StreamConfig, opts ...nats.JSOpt) (*nats.StreamInfo, error)
 	Subscribe(subj string, cb nats.MsgHandler, opts ...nats.SubOpt) (*nats.Subscription, error)
@@ -17,10 +17,12 @@ type SimplifiedJetStream interface {
 	PublishAsyncComplete() <-chan struct{}
 }
 
+// YaloNatsClient is a simplified facade to the JetStream client provided by NATS.
 type YaloNatsClient struct {
 	js SimplifiedJetStream
 }
 
+// Prepare makes sure the client is prepared to send and receive messages.
 func (c *YaloNatsClient) Prepare() error {
 	duration, _ := time.ParseDuration("24h")
 	_, err := c.js.AddStream(&nats.StreamConfig{
@@ -32,25 +34,33 @@ func (c *YaloNatsClient) Prepare() error {
 	return err
 }
 
+// Subscribe asynchronously subscribes to a subject, without a queue group.
+// Every subscriber defined like this will receive the messages for that subject.
 func (c *YaloNatsClient) Subscribe(subj string, cb nats.MsgHandler) (*nats.Subscription, error) {
 	subscription, err := c.js.Subscribe(subj, cb)
 	return subscription, err
 }
 
+// QueueSubscribe asynchronously subscribe to a subject, but within a queue group.
+// Only one subscriber for each queue group will receive the messages for that subject.
 func (c *YaloNatsClient) QueueSubscribe(subj, queue string, cb nats.MsgHandler) (*nats.Subscription, error) {
 	subscription, err := c.js.QueueSubscribe(subj, queue, cb)
 	return subscription, err
 }
 
+// Publish asynchronously publishes a message to a subject.
+// Make sure to call DonePublishing to make sure all messages were published successfully.
 func (c *YaloNatsClient) Publish(subj string, data []byte) (nats.PubAckFuture, error) {
 	paf, err := c.js.PublishAsync(subj, data)
 	return paf, err
 }
 
+// DonePublishing returns a channel that can be used to check whether all messages have been published.
 func (c *YaloNatsClient) DonePublishing() <-chan struct{} {
 	return c.js.PublishAsyncComplete()
 }
 
+// NewNatsClient creates a new instance of YaloNatsClient, with sane defaults.
 func NewNatsClient() (YaloNatsClient, error) {
 	url := os.Getenv("NATS_URL")
 	if url == "" {
