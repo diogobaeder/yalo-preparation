11debd7d13a9aa5e0e6a8214088caf06639c3cec
Diogo Baeder <diogobaeder@yahoo.com.br>
2023-03-11 23:11:58 -0300

    Now parsing the user from the subject, in order to reply to the right subject too.

--- backend/cmd/bot.go
+++ backend/cmd/bot.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/nats-io/nats.go"
 	"log"
+	"regexp"
 	"time"
 	"yalo/diogo/demo/backend/internal/clients"
 )
@@ -11,23 +12,24 @@ import (
 func main() {
 	log.Println("Starting bot...")
 	client, err := clients.NewNatsClient()
+	botSubjectPattern := regexp.MustCompile(`^yalo\.bot\.(?P<user>[^.]+)$`)
 
 	if err != nil {
 		log.Panicf("Couldn't instantiate the client: %v", err)
 	}
 
-	err = client.Prepare()
-
-	if err != nil {
+	if err = client.Prepare(); err != nil {
 		log.Panicf("Couldn't prepare the client: %v", err)
 	}
 
 	log.Println("Subscribing to subject within queue group...")
 	_, err = client.QueueSubscribe("yalo.bot.>", "bots", func(msg *nats.Msg) {
-		log.Printf("Got message: %v", string(msg.Data))
-		_, err := client.Publish("yalo.user.1234", []byte("Got your message!"))
-
-		if err != nil {
+		matches := botSubjectPattern.FindStringSubmatch(msg.Subject)
+		index := botSubjectPattern.SubexpIndex("user")
+		user := matches[index]
+		log.Printf("Got message fro user %v: %v", user, string(msg.Data))
+		userSubject := fmt.Sprintf("yalo.user.%v", user)
+		if _, err := client.Publish(userSubject, []byte("Got your message!")); err != nil {
 			log.Panicf("Couldn't publish message: %v", err)
 		}
 	})
@@ -41,7 +43,7 @@ func main() {
 		select {
 		case <-client.DonePublishing():
 		case <-time.After(5 * time.Second):
-			fmt.Println("Unable to finish publishing messages")
+			log.Panicln("Unable to finish publishing messages")
 		}
 	}
 }
