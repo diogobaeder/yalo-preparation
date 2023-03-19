addfa3f76945da1a645177b3f60503988977ce24
Diogo Baeder <diogobaeder@yahoo.com.br>
2023-03-18 22:58:13 -0300

    First version that works for inserting and querying latest messages for user.

--- backend/internal/clients/scylla.go
+++ backend/internal/clients/scylla.go
@@ -2,26 +2,73 @@ package clients
 
 import (
 	"errors"
+	"fmt"
 	"github.com/gocql/gocql"
 	"github.com/scylladb/gocqlx/v2"
+	"github.com/scylladb/gocqlx/v2/qb"
+	"github.com/scylladb/gocqlx/v2/table"
 	"os"
 	"strings"
+	"time"
 )
+
+var messageMetadata = table.Metadata{
+	Name:    "messages",
+	Columns: []string{"user", "time", "message"},
+	PartKey: []string{"user"},
+	SortKey: []string{"time"},
+}
+
+var messageTable = table.New(messageMetadata)
+
+type Message struct {
+	User    string
+	Time    time.Time
+	Message string
+}
 
 type ScyllaClient struct {
 	session *gocqlx.Session
 }
 
+func (c *ScyllaClient) Insert(message *Message) error {
+	query := c.session.Query(messageTable.Insert()).BindStruct(message)
+	return query.ExecRelease()
+}
+
+func (c *ScyllaClient) Truncate() error {
+	return c.session.ExecStmt(fmt.Sprintf("TRUNCATE %v", messageTable.Name()))
+}
+
+func (c *ScyllaClient) LatestForUser(user string, since time.Time) ([]*Message, error) {
+	var messages []*Message
+	//query := c.session.Query(messageTable.Select()).BindMap(qb.M{
+	//	"user": user,
+	//	"time": qb.GtOrEqLit("time", since.String()),
+	//})
+	query := c.session.Query(qb.Select(messageTable.Name()).Where(
+		qb.EqLit("user", fmt.Sprintf("'%v'", user)),
+		qb.GtOrEqLit("time", since.Format("'2006-01-02 15:04:05.999'")),
+	).ToCql())
+	//log.Fatalln(query.String())
+	if err := query.SelectRelease(&messages); err != nil {
+		return nil, err
+	}
+	return messages, nil
+}
+
 func NewScyllaClient() (*ScyllaClient, error) {
 	urls := os.Getenv("SCYLLA_ADDRS")
-	if urls == "" {
-		return &ScyllaClient{}, errors.New("SCYLLA_ADDRS environment variable not defined")
+	keyspace := os.Getenv("SCYLLA_KEYSPACE")
+	if urls == "" || keyspace == "" {
+		return &ScyllaClient{}, errors.New("both SCYLLA_ADDRS and SCYLLA_KEYSPACE vars should be defined")
 	}
 	hosts := strings.Split(urls, ",")
 	cluster := gocql.NewCluster(hosts...)
+	cluster.Keyspace = keyspace
 	session, err := gocqlx.WrapSession(cluster.CreateSession())
 	if err != nil {
-		return &ScyllaClient{}, errors.New("couldn't create session")
+		return &ScyllaClient{}, err
 	}
 	return &ScyllaClient{&session}, nil
 }
--- backend/internal/clients/scylla_test.go
+++ backend/internal/clients/scylla_test.go
@@ -5,6 +5,7 @@ import (
 	"github.com/stretchr/testify/require"
 	"os"
 	"testing"
+	"time"
 )
 
 func TestStartsWithSession(t *testing.T) {
@@ -17,4 +18,35 @@ func TestStartsWithSession(t *testing.T) {
 
 	ensure.Nilf(err, "Could not instantiate client: %v", err)
 	ensure.IsType(&gocqlx.Session{}, instance.session)
+}
+
+func TestInsertsMessage(t *testing.T) {
+	ensure := require.New(t)
+	if os.Getenv("SCYLLA_ADDRS") == "" {
+		t.Skip("SCYLLA_ADDRS not configured, skipping the test.")
+	}
+	instance, _ := NewScyllaClient()
+	now := time.UnixMilli(time.Now().UnixMilli()).UTC()
+	past := now.Add(time.Hour * -1)
+	message := &Message{
+		"johndoe",
+		now,
+		"Something to say",
+	}
+	ensure.Nil(instance.Truncate())
+
+	err := instance.Insert(message)
+
+	ensure.Nilf(err, "Could not insert message: %v", err)
+
+	retrieved, err := instance.LatestForUser("johndoe", past)
+
+	ensure.Nilf(err, "Could not retrieve messages: %v", err)
+
+	ensure.Equal(1, len(retrieved))
+	ensure.Equal(Message{
+		User:    "johndoe",
+		Time:    now,
+		Message: "Something to say",
+	}, *retrieved[0])
 }
